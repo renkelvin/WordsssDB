8aae86e58685562c92cf62d3bdffa076a6be35b5
ohluyao <fengluyao123@gmail.com>
2011-11-22 14:04:16 +0800

    添加了WordsssDBManger 用来提供 数据库管理接口 初步 实现了 word table的管理接口

---
+++ WordsssDB/WordsssDBManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Odbc;
+
+namespace WordsssDB
+{
+    class WordsssDBManager
+    {
+        string strConn = "Driver={MySQL ODBC 5.1 Driver};Server=localhost;Database=word;User=root;Password=ohluyaomysql";
+        OdbcConnection myConnection;
+        public WordsssDBManager()
+        {
+            myConnection = new OdbcConnection(strConn);
+            myConnection.Open();
+        }
+
+        public void CloseManager()
+        {
+            myConnection.Close();
+        }
+
+        public bool addWord(string word_name)
+        {
+
+
+            string queryMaxId = "select Max(word_id) from word";
+            OdbcCommand myCommand = new OdbcCommand(queryMaxId, myConnection);
+            OdbcDataReader reader = myCommand.ExecuteReader();
+            reader.Read();
+            
+            string addWord = String.Format("insert into word values({0},'{1}')",reader.GetInt32(0)+1,word_name);
+            myCommand = new OdbcCommand(addWord, myConnection);
+            if(myCommand.ExecuteNonQuery() == 1)
+                return true;
+            return false;
+        }
+
+        public bool deleteWord(int word_id)
+        {
+            string deleteSql = String.Format("delete from word where word_id = {0}", word_id);
+            OdbcCommand delCommand = new OdbcCommand(deleteSql, myConnection);
+            if(delCommand.ExecuteNonQuery() == 1)
+                return true;
+            return false;
+        }
+
+        public bool updateWord(int word_id, string word_name)
+        {
+            string updateSql = String.Format(
+                            "update word set word_name = '{0}' where word_id = {1}", word_name, word_id);
+            OdbcCommand updateCommand = new OdbcCommand(updateSql, myConnection);
+
+            if (updateCommand.ExecuteNonQuery() == 1)
+                return true;
+            return false;
+        }
+
+        public string queryWord(int word_id)
+        {
+            string querySql = String.Format("select * from word where word_id = {0}", word_id);
+            OdbcCommand queryCmd = new OdbcCommand(querySql, myConnection);
+            OdbcDataReader reader = queryCmd.ExecuteReader();
+            if (reader.Read())
+            {
+                return String.Format("{0},{1}", reader[0], reader[1]);
+            }
+            return "can't find word";
+        }
+
+        public IEnumerable<string> getAllWord()
+        {
+            string querySql = String.Format("select * from word");
+            OdbcCommand queryCmd = new OdbcCommand(querySql, myConnection);
+            OdbcDataReader reader = queryCmd.ExecuteReader();
+            
+            List<string> resultList = new List<string>();
+            while (reader.Read())
+            {
+                string result = String.Format("{0},{1}", reader[0], reader[1]);
+                resultList.Add(result);
+            }
+            return resultList;
+        }
+    }
+}
