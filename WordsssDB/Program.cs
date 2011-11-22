44c6cf2494070f09cf73df21bf0ddf912ad8cd22
ohluyao <fengluyao123@gmail.com>
2011-11-22 14:02:34 +0800

    创建了 WordsssDBManager 提供数据库管理接口 初步 创建了 管理word table的接口

--- WordsssDB/Program.cs
+++ WordsssDB/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Odbc;
 
 namespace WordsssDB
 {
@@ -9,6 +10,25 @@ namespace WordsssDB
     {
         static void Main(string[] args)
         {
+            WordsssDBManager dbManager = new WordsssDBManager();
+
+            //if (dbManager.addWord("test"))
+             //   Console.WriteLine("添加成功");
+            
+            //if (dbManager.deleteWord(3))
+            //    Console.WriteLine("delete successfully");
+
+            //if (dbManager.updateWord(2,"updateword"))
+            //    Console.WriteLine("update successfully");
+
+            Console.WriteLine(dbManager.queryWord(1));
+
+            var result = dbManager.getAllWord();
+            foreach (var word in result)
+            {
+                Console.WriteLine(word);
+            }
+            dbManager.CloseManager();
         }
     }
 }
